610f8eee1494cc0591bb680bfdf05e137e0cce63
bhavikmk <u19me151@med.svnit.ac.in>
2022-08-20 11:21:08 +0530

    add feature matching

--- src/fruit_sorter/feature_tracking.py
+++ src/fruit_sorter/feature_tracking.py
@@ -94,3 +94,20 @@ img = cv2.drawKeypoints(img, kp, None, (0,255,0),0)
 
 cv2.imshow('orb', img)
 cv2.waitKey(3000)
+
+########### Matching
+
+img1 = img2 = img
+
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+matches = bf.match(des1,des2)
+
+matches = sorted(matches, key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.imshow(img3)
+plt.show()
